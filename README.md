3276f606a1dccf4e574c0607eea82dd5dccbaa68
iwyg <mail@thomas-appel.com>
2015-12-29 17:27:36 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# dockerized php dev environment
---
+++ app/public/index.php
@@ -0,0 +1,18 @@
+<?php
+
+phpinfo();
+#require "../vendor/autoload.php";
+#
+#use Slim\App;
+#use Psr\Http\Message\ResponseInterface as Response;
+#use Psr\Http\Message\ServerRequestInterface as Request;
+#
+#$app = new App;
+#
+#$app->get('/', function (Request $request, Response $response) {
+#    $response->getBody()->write("I now for once welcome our new overloards.");
+#
+#    return $response;
+#});
+#
+#$app->run();
